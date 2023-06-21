bab623a2d260a5e62339b5fae5b6f2d9b471332a
Ahmed Mohamed <am0103738@gmail.com>
2023-06-21 11:47:09 +0300

    Update Audio-Video-Images/merge-audio-folder.sh

--- Audio-Video-Images/merge-audio-folder.sh
+++ Audio-Video-Images/merge-audio-folder.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
-# prompt user to select a folder
-folder=$(zenity --file-selection --directory --title="Select a folder containing audio files")
+# check if folder path is provided as argument
+if [ -z "$1" ]; then
+    echo "Please provide a folder path as argument"
+    exit 1
+fi
+
+# extract folder name from path
+folder_name=$(basename "$1")
 
 # combine all audio files to one file in sorted order
-ffmpeg -i "concat:$(find "$folder" -name '*.mp3' | sort | tr '\n' '|')" -acodec copy output.mp3 > output.log 2>&1
+ffmpeg -i "concat:$(find "$1" -name '*.mp3' | sort | tr '\n' '|')" -acodec copy "$folder_name.mp3" > output.log 2>&1
 
+notify-send "Audio files merged successfully"
