0a2d899f955413b857d7517a900aab7aeb2badea
Ahmed Mohamed <am0103738@gmail.com>
2023-06-21 11:51:30 +0300

    fix to work without Nautilus

--- Audio-Video-Images/Cut Audio - Video/cut_segment.sh
+++ Audio-Video-Images/Cut Audio - Video/cut_segment.sh
@@ -5,34 +5,31 @@
 SEGMENT_LENGTH=60
 
 # Get selected file(s)
-IFS=$'\n'
-selected_files=($NAUTILUS_SCRIPT_SELECTED_FILE_PATHS)
+selected_file="$1"
 
-for selected_file in "${selected_files[@]}"; do
-	# Check if selected file exists
-	if [ ! -f "$selected_file" ]
-	then
-	    zenity --error --text="File '$selected_file' does not exist'"
-	    exit 1
-	fi
+# Check if selected file exists
+if [ ! -f "$selected_file" ]
+then
+	zenity --error --text="File '$selected_file' does not exist'"
+	exit 1
+fi
 
-	# Prompt user for segment length in seconds
-	SEGMENT_LENGTH=$(zenity --entry --title="Split a video or audio file" --text="Enter the desired segment length in seconds (default is 60):")
+# Prompt user for segment length in seconds
+SEGMENT_LENGTH=$(zenity --entry --title="Split a video or audio file" --text="Enter the desired segment length in seconds (default is 60):")
 
-	# Use default segment length of 60 seconds if user did not enter a value
-	if [ -z "$SEGMENT_LENGTH" ]
-	then
-	    SEGMENT_LENGTH=60
-	fi
+# Use default segment length of 60 seconds if user did not enter a value
+if [ -z "$SEGMENT_LENGTH" ]
+then
+	SEGMENT_LENGTH=60
+fi
 
-	# Get the selected file extension
-	FILE_EXTENSION="${selected_file##*.}"
+# Get the selected file extension
+FILE_EXTENSION="${selected_file##*.}"
 
-	# Set the output file prefix
-	OUTPUT_PREFIX="${selected_file%.*}_part"
+# Set the output file prefix
+OUTPUT_PREFIX="${selected_file%.*}_part"
 
-	# Use FFmpeg to split the selected file into segments
-	ffmpeg -i "$selected_file" -c copy -map 0 -segment_time "$SEGMENT_LENGTH" -reset_timestamps 1 -f segment "${OUTPUT_PREFIX}%03d.${FILE_EXTENSION}"
+# Use FFmpeg to split the selected file into segments
+ffmpeg -i "$selected_file" -c copy -map 0 -segment_time "$SEGMENT_LENGTH" -reset_timestamps 1 -f segment "${OUTPUT_PREFIX}%03d.${FILE_EXTENSION}"
 
-	zenity --info --text="File '$selected_file' has been split into ${SEGMENT_LENGTH}-second segments."
-done
+zenity --info --text="File '$selected_file' has been split into ${SEGMENT_LENGTH}-second segments."
