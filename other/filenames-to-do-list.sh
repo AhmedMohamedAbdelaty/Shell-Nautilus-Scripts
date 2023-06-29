d769e70ccf2ea9699e971a7245379cb6bf6760cd
Ahmed Mohamed <am0103738@gmail.com>
2023-06-29 16:11:36 +0300

    new script : folder content to do list

---
+++ other/filenames-to-do-list.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Get directory name from first argument 
+dir_name="$1"
+
+# Create Markdown file with same name 
+md_file="${dir_name}.md"
+touch "$md_file"
+
+# Get all files in that directory 
+files=( "$dir_name"/* )
+
+# Write files to Markdown list  
+echo "# $dir_name To-Do List" >> "$md_file"
+for file in "${files[@]}" 
+do
+    echo "- [ ] ${file##*/}" | sed 's/"//g' >> "$md_file"
+done
+
+# Inform user Markdown file created
+echo "Created $md_file"
