e41a4dcfb4f59d6910b48e47d044479309e1c099
Ahmed Mohamed <am0103738@gmail.com>
2024-01-10 05:11:07 +0200

    Refactor filenames-to-do-list.sh script to create a Markdown to-do list for a given directory

--- other/filenames-to-do-list.sh
+++ other/filenames-to-do-list.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
-# Get directory name from first argument 
+# Get directory name from first argument
 dir_name="$1"
 
-# Create Markdown file with same name 
+# Create Markdown file with same name
 md_file="${dir_name}.md"
 touch "$md_file"
 
-# Get all files in that directory 
-files=( "$dir_name"/* )
+# Get all files in that directory
+files=("$dir_name"/*)
 
-# Write files to Markdown list  
-echo "# $dir_name To-Do List" >> "$md_file"
-for file in "${files[@]}" 
-do
-    echo "- [ ] ${file##*/}" | sed 's/"//g' >> "$md_file"
+# Write files to Markdown list
+echo "# $dir_name To-Do List" >>"$md_file"
+for file in "${files[@]}"; do
+    echo "- [ ] ${file##*/}" | sed 's/"//g' >>"$md_file"
 done
 
-# Inform user Markdown file created
-echo "Created $md_file"
+# Inform user Markdown file create as a notification and open it
+notify-send "To-Do List Created" "$md_file"
+xdg-open "$md_file"
